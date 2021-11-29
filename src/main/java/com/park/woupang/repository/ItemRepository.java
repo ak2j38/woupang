5bc12aba63e9b70da04a2b137f4d154c183f2549
ak2j38 <k2j38@naver.com>
2021-11-29 18:16:37 +0900

    add : ItemRepository.java
    Item 항목을 관리할 수 있는 JPARepository를 상속받음
    
    close

---
+++ src/main/java/com/park/woupang/repository/ItemRepository.java
@@ -0,0 +1,17 @@
+package com.park.woupang.repository;
+
+import com.park.woupang.entity.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface ItemRepository extends JpaRepository<Item, Long>, QuerydslPredicateExecutor<Item> {
+
+    List<Item> findByItemName(String itemName);
+
+    @Query("select i from Item i where i.itemDetail like %:itemDetail% order by i.price desc")
+    List<Item> findByItemDetail(@Param("itemDetail") String itemDetail);
+}
